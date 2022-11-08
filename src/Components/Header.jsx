24d0af25d03472cb7b80b87a7a059c29c0e20dd2
FomichevIgor <96172473+FomichevIgor@users.noreply.github.com>
2022-11-08 16:16:01 +0700

    main components

---
+++ src/Components/Header.jsx
@@ -0,0 +1,11 @@
+
+
+function Header() {
+    return (
+        <header className='header'>
+           
+            <img src="https://cdn-icons-png.flaticon.com/64/4260/4260116.png" alt="logo" />
+        </header>
+    )
+}
+export default Header;
---
+++ src/Components/Profile.jsx
@@ -0,0 +1,15 @@
+function Profile() {
+    return(
+        <div className='content'> <div> <img src="https://avatars.mds.yandex.net/i?id=7cfba74f3cb75ed85ad2b2a55b9b039436287ce4-6274710-images-thumbs&n=13" alt="" />
+        </div>
+
+          <div>ava+description</div>
+          <div>My post</div>
+          <div>New post</div>
+      <div></div>
+        </div>
+
+
+    )
+}
+export default Profile;
